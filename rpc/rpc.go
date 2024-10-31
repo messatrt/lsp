9ce4844148e86d1ab5060d2d27f419537ba0e8f6
messatrt <essaessa9068@gmail.com>
2024-10-31 11:11:24 +0530

    Creating the decode function

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,14 +1,24 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
-func EncodeMessage(msg any)string{
+func EncodeMessage(msg any) string {
 	content, err := json.Marshal(msg)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
-	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s",len(content), content)
+	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) error {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return errors.New("Sperateor not found")
+	}
+	return nil
 }
